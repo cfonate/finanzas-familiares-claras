07cac0197c2b4d22297443056214b3d880cfc4ce
cfonate <cfonate@gmail.com>
2025-05-17 19:37:52 +0200

    Update section6.ts

--- src/data/sections/section6.ts
+++ src/data/sections/section6.ts
@@ -8,7 +8,7 @@ export const section6: Section = {
   questions: [
     {
       id: 91,
-      text: "¿Tienen algún seguro de vida?",
+      text: "¿Tenéis algún seguro de vida?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo uno de los dos miembros", value: "b", points: 2 },
@@ -18,7 +18,7 @@ export const section6: Section = {
     },
     {
       id: 92,
-      text: "¿Cuánto pagan mensualmente por su seguro de vida (en total)?",
+      text: "¿Cuánto pagáis mensualmente por vuestro seguro de vida (en total)?",
       options: [
         { label: "a) Nada", value: "a", points: 1 },
         { label: "b) Menos de 20 €", value: "b", points: 2 },
@@ -28,7 +28,7 @@ export const section6: Section = {
     },
     {
       id: 93,
-      text: "¿Tienen seguro de salud privado?",
+      text: "¿Tenéis seguro de salud privado?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo uno de los miembros", value: "b", points: 2 },
@@ -38,7 +38,7 @@ export const section6: Section = {
     },
     {
       id: 94,
-      text: "¿Cuánto pagan mensualmente por su seguro de salud privado (en total)?",
+      text: "¿Cuánto pagáis mensualmente por vuestro seguro de salud privado (en total)?",
       options: [
         { label: "a) Nada", value: "a", points: 1 },
         { label: "b) Menos de 50 €", value: "b", points: 2 },
@@ -48,7 +48,7 @@ export const section6: Section = {
     },
     {
       id: 95,
-      text: "¿Tienen seguro de hogar?",
+      text: "¿Tenéis seguro de hogar?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo la vivienda", value: "b", points: 2 },
@@ -58,7 +58,7 @@ export const section6: Section = {
     },
     {
       id: 96,
-      text: "¿Cuánto pagan mensualmente por su seguro de hogar?",
+      text: "¿Cuánto pagan mensualmente por vuestr seguro de hogar?",
       options: [
         { label: "a) Nada", value: "a", points: 1 },
         { label: "b) Menos de 20 €", value: "b", points: 2 },
@@ -68,7 +68,7 @@ export const section6: Section = {
     },
     {
       id: 97,
-      text: "¿Tienen seguro de coche?",
+      text: "¿Tenéis seguro de coche?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo a terceros", value: "b", points: 2 },
@@ -78,7 +78,7 @@ export const section6: Section = {
     },
     {
       id: 98,
-      text: "¿Cuánto pagan mensualmente por su seguro de coche?",
+      text: "¿Cuánto pagáis mensualmente por vuestro seguro de coche?",
       options: [
         { label: "a) Menos de 30 €", value: "a", points: 4 },
         { label: "b) Entre 30 € y 60 €", value: "b", points: 3 },
@@ -88,7 +88,7 @@ export const section6: Section = {
     },
     {
       id: 99,
-      text: "¿Tienen algún seguro de incapacidad o accidente?",
+      text: "¿Tenéis algún seguro de incapacidad o accidente?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo uno de los miembros", value: "b", points: 2 },
@@ -98,7 +98,7 @@ export const section6: Section = {
     },
     {
       id: 100,
-      text: "¿Cuánto pagan mensualmente por su seguro de incapacidad o accidente?",
+      text: "¿Cuánto pagáis mensualmente por vuestro seguro de incapacidad o accidente?",
       options: [
         { label: "a) Nada", value: "a", points: 1 },
         { label: "b) Menos de 20 €", value: "b", points: 2 },
@@ -108,7 +108,7 @@ export const section6: Section = {
     },
     {
       id: 101,
-      text: "¿Tienen algún tipo de seguro de ahorro o inversión?",
+      text: "¿Tenéis algún tipo de seguro de ahorro o inversión?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, pero con pocos aportes", value: "b", points: 2 },
@@ -118,7 +118,7 @@ export const section6: Section = {
     },
     {
       id: 102,
-      text: "¿Tienen un seguro de vida vinculado a la hipoteca?",
+      text: "¿Tenéis un seguro de vida vinculado a la hipoteca?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo uno de los miembros", value: "b", points: 2 },
@@ -128,7 +128,7 @@ export const section6: Section = {
     },
     {
       id: 103,
-      text: "¿Tienen algún seguro de decesos?",
+      text: "¿Tenéis algún seguro de decesos?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo uno de los miembros", value: "b", points: 2 },
@@ -138,7 +138,7 @@ export const section6: Section = {
     },
     {
       id: 104,
-      text: "¿Cuánto pagan mensualmente por su seguro de decesos?",
+      text: "¿Cuánto pagáis mensualmente por vuestro seguro de decesos?",
       options: [
         { label: "a) Nada", value: "a", points: 1 },
         { label: "b) Menos de 10 €", value: "b", points: 2 },
@@ -148,7 +148,7 @@ export const section6: Section = {
     },
     {
       id: 105,
-      text: "¿Han revisado sus pólizas de seguros en el último año para ajustar coberturas?",
+      text: "¿Habéis revisado vuestras pólizas de seguros en el último año para ajustar coberturas?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo el seguro de hogar", value: "b", points: 2 },
@@ -158,7 +158,7 @@ export const section6: Section = {
     },
     {
       id: 106,
-      text: "¿Tienen un seguro para la educación de los hijos?",
+      text: "¿Tenéis un seguro para la educación de los hijos?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, con aportes esporádicos", value: "b", points: 2 },
@@ -168,7 +168,7 @@ export const section6: Section = {
     },
     {
       id: 107,
-      text: "¿Tienen algún seguro para gastos médicos imprevistos no cubiertos por el sistema público?",
+      text: "¿Tenéis algún seguro para gastos médicos imprevistos no cubiertos por el sistema público?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, con una cobertura básica", value: "b", points: 2 },
@@ -178,7 +178,7 @@ export const section6: Section = {
     },
     {
       id: 108,
-      text: "¿Tienen algún seguro de asistencia en viaje o internacional?",
+      text: "¿Tenéis algún seguro de asistencia en viaje o internacional?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, solo para viajes largos", value: "b", points: 2 },
@@ -188,7 +188,7 @@ export const section6: Section = {
     },
     {
       id: 109,
-      text: "¿Han tenido alguna vez que usar alguno de los seguros que poseen?",
+      text: "¿Habéis tenido alguna vez que usar alguno de los seguros?",
       options: [
         { label: "a) No", value: "a", points: 1 },
         { label: "b) Sí, una vez", value: "b", points: 2 },
@@ -198,7 +198,7 @@ export const section6: Section = {
     },
     {
       id: 110,
-      text: "¿Cómo calificarían la relación calidad-precio de sus seguros?",
+      text: "¿Cómo calificáis la relación calidad-precio de vuestros seguros?",
       options: [
         { label: "a) Muy mala", value: "a", points: 1 },
         { label: "b) Regular", value: "b", points: 2 },
