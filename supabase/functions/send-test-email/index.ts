ee4fadf7542e1974c428690adac5b84849477489
cfonate <cfonate@gmail.com>
2025-05-20 15:22:48 +0200

    Confirmar

--- supabase/functions/send-test-email/index.ts
+++ supabase/functions/send-test-email/index.ts
@@ -2,7 +2,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { Resend } from "npm:resend@2.0.0";
 
-const resend = new Resend(Deno.env.get("RESEND_API_KEY"));
+const resend = new Resend(Deno.env.get("re_B8iLBh2r_PNz7QeURbi594f7eHaugRtSS"));
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
